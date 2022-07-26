976e787081c1c53d3f7bdd237681f23bb5bf0124
MaxTsh <tatshahdoost@gmail.com>
2022-07-27 01:26:19 +0430

    added whatwg-fetch imported to setup test

--- src/mocks/setup.ts
+++ src/mocks/setup.ts
@@ -1,5 +1,6 @@
 import { beforeAll, afterAll, afterEach } from "vitest";
 import "@testing-library/jest-dom";
+import "whatwg-fetch";
 import { server } from "./server";
 
 beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
