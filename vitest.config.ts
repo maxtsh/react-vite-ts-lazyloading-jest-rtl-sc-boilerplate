e3e274b5275e7b1d30fd3cd414e13fdbd3e7c698
MaxTsh <tatshahdoost@gmail.com>
2022-07-27 01:53:49 +0430

    added resolve to vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,29 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: "containers",
+        replacement: path.resolve(__dirname, "src/containers"),
+      },
+      {
+        find: "components",
+        replacement: path.resolve(__dirname, "src/components"),
+      },
+      {
+        find: "utils",
+        replacement: path.resolve(__dirname, "src/utils"),
+      },
+      {
+        find: "styles",
+        replacement: path.resolve(__dirname, "src/styles"),
+      },
+    ],
+  },
   test: {
     /* for example, use global to avoid globals imports (describe, test, expect): */
     globals: true,
